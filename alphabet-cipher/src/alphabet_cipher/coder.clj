a9ce3c01108413ef84044b2c46a70a5b926be9a6
Pedro Galan <pedro.galan@piksel.com>
2020-04-27 08:18:31 +0100

    First implementation. Bad handle of keyword - needs improving

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,17 @@
 (ns alphabet-cipher.coder)
 
+(defn letter-from-matrix [x y]
+  (let [char-to-int #(- (int %) (int \a))
+        n           (char-to-int x)
+        m           (char-to-int y)]
+    (char (+ (int \a) (rem (+ n m) 26)))))
+
 (defn encode [keyword message]
-  "encodeme")
+  (let [k (apply str (repeat 10 keyword))]
+    (apply str (map letter-from-matrix k message))))
 
 (defn decode [keyword message]
   "decodeme")
 
 (defn decipher [cipher message]
   "decypherme")
-
