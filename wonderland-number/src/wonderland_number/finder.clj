7399dfaffed9d5c7ca8b22af19c0ee4c2135476f
Pedro Galan <pedro.galan@piksel.com>
2020-04-28 17:15:36 +0100

    First version or Wonderland number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,16 @@
 (ns wonderland-number.finder)
 
+(defn- same-digits? [n m]
+  (let [n-digits (-> n str set)
+        m-digits (-> m str set)]
+    (= n-digits m-digits)))
+
+(defn- wonderland-number? [n]
+  (and (same-digits? n (* 2 n))
+       (same-digits? n (* 3 n))
+       (same-digits? n (* 4 n))
+       (same-digits? n (* 5 n))
+       (same-digits? n (* 6 n))))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first (filter wonderland-number? (range 100000 999999))))
